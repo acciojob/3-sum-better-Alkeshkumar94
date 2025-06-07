cc0490752bbac385940652b33c48e16f39844aad
Alkesh kumar <127075190+Alkeshkumar94@users.noreply.github.com>
2025-06-07 21:15:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 function threeSum(arr, target) {
 // write your code here
 	let n=arr.length;
-	let ans=infinity;
+	let ans=Infinity;
 	for(let i=0;i<n-2;i++){
 		for(let j=i+1;j<n-1;j++){
 			for(let k=j+1;k<n;k++){
 				let sum=arr[i]+arr[j]+arr[k];
-				if(math.abs(sum-target)<ans){
+				if(Math.abs(sum-target)<ans){
 					ans=sum;
 				}
 			}
